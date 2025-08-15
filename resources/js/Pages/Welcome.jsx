4091700143c8a49fd5b7d03cb15fa2fe37c0cf01
[nurwahyuam] <[nurwahyuam11@gmail.com]>
2025-08-15 08:51:10 +0700

    Maps Landing

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,39 +1,48 @@
 import { Head } from "@inertiajs/react";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-export default function Welcome({ auth }) {
-    useEffect(() => {
-        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-            attribution:
-                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        }).addTo(map);
+export default function Welcome() {
+  const mapRef = useRef(null);
 
-        const cities = [
-            { name: "Jakarta", coords: [-6.200000, 106.816666] },
-            { name: "Surabaya", coords: [-7.257472, 112.752090] },
-            { name: "Bandung", coords: [-6.917464, 107.619125] },
-            { name: "Yogyakarta", coords: [-7.795580, 110.369492] },
-        ];
+  useEffect(() => {
+    if (mapRef.current) return;
 
-        cities.forEach((city) => {
-            L.marker(city.coords)
-                .addTo(map)
-                .bindPopup(`<b>${city.name}</b>`);
-        });
+    const mapInstance = L.map("map").setView([-7.5, 112.5], 7);
+    mapRef.current = mapInstance;
 
-        return () => {
-            map.remove();
-        };
-    }, []);
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(mapInstance);
 
-    return (
-        <>
-            <Head title="Welcome" />
-            <div className="min-h-screen bg-white w-full flex flex-col items-center justify-center">
-                <div id="map" className="w-full h-screen rounded-lg shadow-lg"></div>
-            </div>
-        </>
-    );
+    fetch("https://z4nr.github.io/WhatGeo/api/?type=kota&province=35")
+      .then(res => res.json())
+      .then(data => {
+        L.geoJSON(data, {
+          style: () => ({
+            fillColor: "#74c476",
+            color: "#238b45",
+            weight: 1,
+            fillOpacity: 0.6,
+          }),
+          onEachFeature: (feature, layer) => {
+            layer.bindPopup(feature.properties.name || "Wilayah");
+          }
+        }).addTo(mapInstance);
+      });
+
+    return () => {
+      mapInstance.remove();
+    };
+  }, []);
+
+  return (
+    <>
+      <Head title="Peta Kota/Kabupaten Jawa Timur" />
+      <div className="min-h-screen bg-white">
+        <div id="map" className="w-full h-screen"></div>
+      </div>
+    </>
+  );
 }
