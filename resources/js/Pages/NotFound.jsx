71a81557557b88b828134ee1172041931769fc25
Natasya <23082010074@student.upnjatim.ac.id>
2025-08-27 14:52:15 +0700

    404

---
+++ resources/js/Pages/NotFound.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function NotFound (){
+    return (
+        <>
+        <div className="flex flex-col items-center justify-center min-h-screen text-center bg-gradient-to-br from-indigo-400 via-indigo-300 to-indigo-200">
+            <h1 className="font-bold text-[200px] leading-none text-white">404</h1>
+            <p className="text-[30px] leading-tight text-white">Halaman yang anda cari tidak ada!</p>
+            <a 
+            href={route("welcome")}
+            className="bg-white mt-5 text-2xl px-5 py-3 rounded-full 
+                        hover:bg-gradient-to-r hover:from-indigo-500 hover:to-indigo-400 
+                        hover:text-white transition duration-300 ease-in-out transform hover:scale-105"
+            >
+            kembali ke halaman utama
+            </a>
+        </div>
+        </>
+    );
+}
